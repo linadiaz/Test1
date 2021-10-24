465413b020eae5eeaadc0c12c2940320feaee2dc
linadiaz <linisdr1@gmail.com>
2021-10-24 07:05:08 -0400

    This is the first version

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Helloworld
+//
+//  Created by lina diaz on 10/24/21.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout<<"Hello World" <<endl;
+ 
+    return 0;
+}
